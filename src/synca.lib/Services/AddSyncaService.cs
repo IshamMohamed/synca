e1d2c52d14dcf2a18daf6abd31e7190e3c5c6a61
Isham Mohamed <isham4you@gmail.com>
2020-07-22 13:58:16 +0800

    Modifed both AddSynca and AddSyncaDistributed to support MemoryCacheOptions

--- src/synca.lib/Services/AddSyncaService.cs
+++ src/synca.lib/Services/AddSyncaService.cs
@@ -1,3 +1,5 @@
+using System;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using synca.lib.Background;
 using synca.lib.Hosted.Service;
@@ -14,9 +16,25 @@ namespace synca.lib.Services
             return services;
         }
 
+        public static IServiceCollection AddSynca(this IServiceCollection services, Action<MemoryCacheOptions> setupAction)
+        {
+            services.AddMemoryCache(setupAction);
+            services.AddSingleton<IBackgroundTaskQueue, BackgroundTaskQueue>();
+            services.AddHostedService<QueuedHostedService>();
+            return services;
+        }
+
         public static IServiceCollection AddSyncaDistributed(this IServiceCollection services)
         {
             services.AddDistributedMemoryCache();
+            services.AddSingleton<IBackgroundTaskQueue, BackgroundTaskQueue>();
+            services.AddHostedService<QueuedHostedService>();
+            return services;
+        }
+
+        public static IServiceCollection AddSyncaDistributed(this IServiceCollection services, Action<MemoryCacheOptions> setupAction)
+        {
+            services.AddDistributedMemoryCache(setupAction);
             services.AddSingleton<IBackgroundTaskQueue, BackgroundTaskQueue>();
             services.AddHostedService<QueuedHostedService>();
             return services;
